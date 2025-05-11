bf2624ce99de2a0b59056ec0d542abad8131ff18
ye-long-dragon <bvincelawrence@gmail.com>
2025-05-12 02:20:51 +0800

    HTML&CSS-100% JS-30% DESIGN-25% ANIMATION-0% LOGIC-10%

---
+++ script.js
@@ -0,0 +1,146 @@
+function educButtonChange(element){
+    let A1 = document.getElementById(A1);
+    let B2 = document.getElementById(B2);
+    let C3 = document.getElementById(C3);
+    let D4 = document.getElementById(D4);
+    
+
+    let a1 = `
+            <div class="block2 line">
+                <div class="circle"></div>
+                <div class="line content"></div>
+            </div>
+            <div class="TrueContent">
+                <div class="place">MAPUA MALAYAN COLLEGES MINDANAO</div>
+                <div class="title">COLLEGE - COMPUTER ENGINEERING</div>
+                <div class="date">2023 - 2028</div>
+                <div class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+            </div>
+            `;
+
+
+    let b2 = `
+            <div class="block2 contentContainer" id="B2">
+                <div class="block2 line">
+                    <div class="circle"></div>
+                    <div class="line content"></div>
+                </div>
+                <div class="TrueContent">
+                    <div class="place">MAPUA MALAYAN COLLEGES MINDANAO</div>
+                    <div class="title">SENIOR HIGH SCHOOL - STEM</div>
+                    <div class="date">2021 - 2023</div>
+                    <div class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+                </div>
+            </div>
+            `;
+
+    let c3 = `
+            <div class="block2 contentContainer" id="C3">
+                <div class="block2 line">
+                    <div class="circle"></div>
+                    <div class="line content"></div>
+                </div>
+                <div class="TrueContent">
+                    <div class="place">SAN PEDRO COLLEGES BED</div>
+                    <div class="title">JUNIOR HIGH SCHOOL</div>
+                    <div class="date">2017 - 2020</div>
+                    <div class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+                </div>
+            </div>
+            `;
+
+    let d4 = `
+            <div class="block2 contentContainer" id="D4">
+                <div class="block2 line">
+                    <div class="circle"></div>
+                    <div class="line content"></div>
+                </div>
+                <div class="TrueContent">
+                    <div class="place">SAN PEDRO COLLEGES BED</div>
+                    <div class="title">GRADESCHOOL</div>
+                    <div class="date">2012 - 2016</div>
+                    <div class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+                </div>
+            </div>
+            `;
+
+    document.getElementById(A1).innerHTML = a1;
+    document.getElementById(B2).innerHTML = b2;
+    document.getElementById(C3).innerHTML = c3;
+    document.getElementById(D4).innerHTML = d4;
+
+}
+    
+
+function workButtonChange(element){
+    let A1 = document.getElementById(A1);
+    let B2 = document.getElementById(B2);
+    let C3 = document.getElementById(C3);
+    let D4 = document.getElementById(D4);
+    let E5 = document.getElementById(e5);
+
+    let a1 = `
+            <div class="block2 line">
+                <div class="circle"></div>
+                <div class="line content"></div>
+            </div>
+            <div class="TrueContent">
+                <div class="place">MAPUA MALAYAN COLLEGES MINDANAO</div>
+                <div class="title">COLLEGE - COMPUTER ENGINEERING</div>
+                <div class="date">2023 - 2028</div>
+                <div class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+            </div>
+            `;
+
+
+    let b2 = `
+            <div class="block2 contentContainer" id="B2">
+                <div class="block2 line">
+                    <div class="circle"></div>
+                    <div class="line content"></div>
+                </div>
+                <div class="TrueContent">
+                    <div class="place">MAPUA MALAYAN COLLEGES MINDANAO</div>
+                    <div class="title">SENIOR HIGH SCHOOL - STEM</div>
+                    <div class="date">2021 - 2023</div>
+                    <div class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+                </div>
+            </div>
+            `;
+
+    let c3 = `
+            <div class="block2 contentContainer" id="C3">
+                <div class="block2 line">
+                    <div class="circle"></div>
+                    <div class="line content"></div>
+                </div>
+                <div class="TrueContent">
+                    <div class="place">SAN PEDRO COLLEGES BED</div>
+                    <div class="title">JUNIOR HIGH SCHOOL</div>
+                    <div class="date">2017 - 2020</div>
+                    <div class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+                </div>
+            </div>
+            `;
+
+    let d4 = `
+            <div class="block2 contentContainer" id="D4">
+                <div class="block2 line">
+                    <div class="circle"></div>
+                    <div class="line content"></div>
+                </div>
+                <div class="TrueContent">
+                    <div class="place">SAN PEDRO COLLEGES BED</div>
+                    <div class="title">GRADESCHOOL</div>
+                    <div class="date">2012 - 2016</div>
+                    <div class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+                </div>
+            </div>
+            `;
+
+    document.getElementById(A1).innerHTML = a1;
+    document.getElementById(B2).innerHTML = b2;
+    document.getElementById(C3).innerHTML = c3;
+    document.getElementById(D4).innerHTML = d4;
+    document.getElementById(E5).innerHTML = e5;
+}
